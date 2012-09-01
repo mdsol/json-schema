f306f2182503641b8e9dccdf983609cc911e4202
Ethan <ethan@medidata>
2012-09-01 18:04:11 -0400

    update test test_extends_and_additionalProperties.rb and the schemas it relies on to not use the removed capability to load any file from the local filesystem

--- test/test_extends_and_additionalProperties.rb
+++ test/test_extends_and_additionalProperties.rb
@@ -3,12 +3,11 @@ require File.dirname(__FILE__) + '/../lib/json-schema'
 
 class ExtendsNestedTest < Test::Unit::TestCase
 
-  def assert_validity(valid, schema_name, data, msg=nil)
-    file = File.expand_path("../schemas/#{schema_name}.schema.json",__FILE__)
-    errors = JSON::Validator.fully_validate file, data
+  def assert_validity(valid, schema, data, msg=nil)
+    errors = JSON::Validator.fully_validate schema, data
     msg.sub! /\.$/, '' if msg
     send (valid ? :assert_equal : :refute_equal), [], errors, \
-      "Schema should be #{valid ? :valid : :invalid}#{msg ? ".\n[#{schema_name}] #{msg}" : ''}"
+      "Schema should be #{valid ? :valid : :invalid}#{msg ? ".\n[#{schema.inspect}] #{msg}" : ''}"
   end
 
   def assert_valid(schema_name, data, msg=nil) assert_validity true, schema_name, data, msg end
@@ -20,25 +19,30 @@ class ExtendsNestedTest < Test::Unit::TestCase
   ].each do |schema_name|
     test_prefix= 'test_' + schema_name.gsub('-','_')
 
+    JSON::Validator.cache_schemas=true
+    JSON::Validator.add_schema(JSON::Schema.new(JSON.parse(File.read(File.expand_path("../schemas/inner.schema.json", __FILE__))), nil))
+
+    schema = JSON.parse(File.read(File.expand_path("../schemas/#{schema_name}.schema.json", __FILE__)))
+
     define_method("#{test_prefix}_valid_outer") do
-      assert_valid schema_name, {"outerC"=>true}, "Outer defn is broken, maybe the outer extends overrode it?"
+      assert_valid schema, {"outerC"=>true}, "Outer defn is broken, maybe the outer extends overrode it?"
     end
 
     define_method("#{test_prefix}_valid_outer_extended") do
-      assert_valid schema_name, {"innerA"=>true}, "Extends at the root level isn't working."
+      assert_valid schema, {"innerA"=>true}, "Extends at the root level isn't working."
     end
 
     define_method("#{test_prefix}_valid_inner") do
-      assert_valid schema_name, {"outerB"=>[{"innerA"=>true}]}, "Extends isn't working in the array element defn."
+      assert_valid schema, {"outerB"=>[{"innerA"=>true}]}, "Extends isn't working in the array element defn."
     end
 
     define_method("#{test_prefix}_invalid_inner") do
-      refute_valid schema_name, {"outerB"=>[{"whaaaaat"=>true}]}, "Array element defn allowing anything when it should only allow what's in inner.schema"
+      refute_valid schema, {"outerB"=>[{"whaaaaat"=>true}]}, "Array element defn allowing anything when it should only allow what's in inner.schema"
     end
 
     if schema_name['extends_and_additionalProperties-1']
       define_method("#{test_prefix}_invalid_outer") do
-        refute_valid schema_name, {"whaaaaat"=>true}, "Outer defn allowing anything when it shouldn't."
+        refute_valid schema, {"whaaaaat"=>true}, "Outer defn allowing anything when it shouldn't."
       end
     end
 
